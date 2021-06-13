71cd9b11ef3572e0dad0a677d9a73a90e6ea4e38
Ivan <waizord@yandex.ru>
2021-06-13 04:46:20 +0300

    Fix: fixed exercise-1

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,20 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        if prices.isEmpty || prices.count == 1{
+            return 0
+        }
+        var result = 0
+        var dropElements = 0
+        prices.forEach { (price) in
+            dropElements = dropElements + 1
+            let newPrices = prices.dropFirst(dropElements)
+            guard let maxPrice = newPrices.max() else {return}
+            
+            if price < maxPrice {
+                result = result + maxPrice - price
+            }
+        }
+        return result
     }
 }
