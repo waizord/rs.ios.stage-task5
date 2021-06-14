d5de1fd3472e9711ab470db5ae6b2bd68a0f8ed2
Ivan <waizord@yandex.ru>
2021-06-14 23:43:04 +0300

    Fix: fixed exercise-2

--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -2,7 +2,48 @@ import UIKit
 
 class MessageDecryptor: NSObject {
     
+    var result = ""
+    var subString = ""
+    var index = [String]()
+    var repeatString = [String]()
+    
     func decryptMessage(_ message: String) -> String {
-        return ""
+    
+        message.forEach {(char) in
+            
+                if char.isNumber {
+                    subString = "\(subString)\(char)"
+                    
+                    print(subString)
+                    
+                } else if char == "[" {
+                    if subString == "" {
+                        index.append("1")
+                    } else {
+                        index.append(subString)
+                    }
+                    subString = ""
+                    repeatString.append(result)
+                    result = ""
+                    
+                    print(repeatString)
+                    
+                } else if char == "]" {
+                    var sentence = ""
+                    if let counts = Int(index.popLast() ?? "1") {
+                        
+                        print(counts)
+                        
+                        for _ in 0..<counts {
+                            sentence += result
+                        }
+                    }
+                    result = repeatString.popLast()! + sentence
+                } else if char.isLetter {
+                    result += String(char)
+                }
+            }
+            
+            return result
     }
 }
